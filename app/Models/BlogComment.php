da13ed2310e10d7107109403e116e0b1e4e8c386
sasithhansaka <sasithhansaka285@gmail.com>
2025-08-28 01:15:53 +0530

    Add fillable properties and relationships to BlogComment model

--- app/Models/BlogComment.php
+++ app/Models/BlogComment.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class BlogComment extends Model
 {
     //
+    protected $fillable = [
+        'content',
+        'user_id',
+        'blog_id',
+    ];
+
+     public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function blog()
+    {
+        return $this->belongsTo(Blog::class);
+    }
 }
